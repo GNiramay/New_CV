a612772f148fd6072d6f40875746d19798dcedad
GNiramay <niramaygogate@gmail.com>
2026-04-05 23:16:46 -0500

    More efficient way to compile.

--- Compile.sh
+++ Compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-lualatex --halt-on-error resume.tex > /dev/null
-rm -f resume.aux resume.log resume.out
+
+# # Basic execution.
+# lualatex --halt-on-error resume.tex > /dev/null
+# rm -f resume.aux resume.log resume.out
+
+# Expected to be more effective
+latexmk -lualatex resume.tex
+rm -f resume.aux resume.fdb_latexmk resume.fls resume.log resume.out

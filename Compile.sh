404a48656173e43dac29dd6ea869b44f432e2756
GNiramay <niramaygogate@gmail.com>
2026-04-11 20:34:19 -0500

    Hpefully final piece for automation

--- Compile.sh
+++ Compile.sh
@@ -4,6 +4,9 @@
 # lualatex --halt-on-error resume.tex > /dev/null
 # rm -f resume.aux resume.log resume.out
 
-# Expected to be more effective
+# Taking care of personal information
+cp -f Personal/contacts_actual.tex Personal/contacts.tex
 latexmk -lualatex resume.tex
 rm -f resume.aux resume.fdb_latexmk resume.fls resume.log resume.out
+mv -f resume.pdf Personal/
+git restore resume.pdf Personal/contacts.tex
